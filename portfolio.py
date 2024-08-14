7ab778aefc2580e7139e8abbed3e640aef2c3d0b
Iqbal Kurriana Putra <104598588+192410101033@users.noreply.github.com>
2024-08-14 16:48:46 +0700

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -117,7 +117,7 @@ if selected == 'About':
         - Business Application Solution Architect - PT ASDP Indonesia Ferry
         """)
     with st.container():
-        st.markdown("""
+        st.link_button("""
             <style>
             .center-button {
                 display: flex;
